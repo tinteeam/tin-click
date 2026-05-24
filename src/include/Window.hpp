c5746e684ef314f662b7aadbf32921182231efba
randomusert <127417963+randomusert@users.noreply.github.com>
2026-05-24 12:28:03 +0300

    Work on D2D; add few libraries to Cmake target_link_libraries

--- src/include/Window.hpp
+++ src/include/Window.hpp
@@ -10,6 +10,12 @@
 
 #include <Windows.h>
 
+//Direct2D(D2D) headers includes
+#include <d2d1.h>
+#include <d2d1helper.h>
+#include <dwrite.h>
+#include <wincodec.h>
+
 namespace tin_click_inc {
 
     class MainWindow {
@@ -47,5 +53,77 @@ inline void SafeRelease(
 EXTERN_C IMAGE_DOS_HEADER __ImageBase;
 #define HINST_THISCOMPONENT ((HINSTANCE)&__ImageBase)
 #endif
+
+
+class GameApp
+{
+public:
+    GameApp();
+    ~GameApp();
+
+    // Register the window class and call methods for instantiating drawing resources
+    HRESULT Initialize();
+
+    // Process and dispatch messages
+    void RunMessageLoop();
+
+private:
+    HRESULT CreateDeviceIndependentResources();
+    HRESULT CreateDeviceResources();
+
+    HRESULT CreateGridPatternBrush(
+        ID2D1RenderTarget* pRenderTarget,
+        ID2D1BitmapBrush** ppBitmapBrush
+    );
+
+    void DiscardDeviceResources();
+
+    HRESULT OnRender();
+
+    void OnResize(
+        UINT width,
+        UINT height
+    );
+
+    static LRESULT CALLBACK WndProc(
+        HWND hWnd,
+        UINT message,
+        WPARAM wParam,
+        LPARAM lParam
+    );
+
+    HRESULT LoadResourceBitmap(
+        ID2D1RenderTarget* pRenderTarget,
+        IWICImagingFactory* pIWICFactory,
+        PCWSTR resourceName,
+        PCWSTR resourceType,
+        UINT destinationWidth,
+        UINT destinationHeight,
+        ID2D1Bitmap** ppBitmap
+    );
+
+    HRESULT LoadBitmapFromFile(
+        ID2D1RenderTarget* pRenderTarget,
+        IWICImagingFactory* pIWICFactory,
+        PCWSTR uri,
+        UINT destinationWidth,
+        UINT destinationHeight,
+        ID2D1Bitmap** ppBitmap
+    );
+
+private:
+    HWND m_hwnd;
+    ID2D1Factory* m_pD2DFactory;
+    IWICImagingFactory* m_pWICFactory;
+    IDWriteFactory* m_pDWriteFactory;
+    ID2D1HwndRenderTarget* m_pRenderTarget;
+    IDWriteTextFormat* m_pTextFormat;
+    ID2D1PathGeometry* m_pPathGeometry;
+    ID2D1LinearGradientBrush* m_pLinearGradientBrush;
+    ID2D1SolidColorBrush* m_pBlackBrush;
+    ID2D1BitmapBrush* m_pGridPatternBitmapBrush;
+    ID2D1Bitmap* m_pBitmap;
+    ID2D1Bitmap* m_pAnotherBitmap;
+};
 
 #endif
